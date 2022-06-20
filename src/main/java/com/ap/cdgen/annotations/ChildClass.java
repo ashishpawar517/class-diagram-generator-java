00152ee0aaa218fe8620059c02a1be69b873f6be
Ashish <acpawar_b18@it.vjti.ac.in>
2022-06-20 15:24:33 +0530

    added annotations

---
+++ src/main/java/com/ap/cdgen/annotations/ChildClass.java
@@ -0,0 +1,12 @@
+package src.main.java.com.ap.cdgen.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ChildClass {
+  public String superClass();
+}
---
+++ src/main/java/com/ap/cdgen/annotations/SuperClass.java
@@ -0,0 +1,11 @@
+package src.main.java.com.ap.cdgen.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SuperClass {
+}
