a944df49862fa6b26a70b9803eb74d6fd8c8211a
Ashish <acpawar_b18@it.vjti.ac.in>
2022-06-20 15:25:21 +0530

    Added main class

---
+++ src/main/java/com/ap/cdgen/Main.java
@@ -0,0 +1,13 @@
+package src.main.java.com.ap.cdgen;
+
+import src.main.java.com.ap.cdgen.utils.FileUtils;
+import src.main.java.com.ap.cdgen.utils.GraphUtils;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String packagePath = "src.main.java.com.ap.cdgen.source";
+    String markdownCode = GraphUtils.createGraph(packagePath);
+    FileUtils.writeToMarkdownFile("sample2.md", markdownCode);
+  }
+}
